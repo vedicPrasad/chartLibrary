6732c42e0f604ab91a5d3970bf24d916d3baf42a
prasadvetkar <prasad.vetkar@vedicrishiastro.com>
2026-06-16 14:43:19 +0530

    main

--- vedicchart/build.gradle.kts
+++ vedicchart/build.gradle.kts
@@ -1,7 +1,11 @@
 plugins {
-    alias(libs.plugins.android.library)
+    id("com.android.library")
+    id("maven-publish")
     alias(libs.plugins.kotlin.compose)
 }
+
+group = "com.github.YOUR_GITHUB_USERNAME"
+version = "1.0.0"
 
 android {
     namespace = "com.vedicrishiastro.vedicchart"
@@ -17,18 +21,24 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     buildFeatures {
         compose = true
     }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
 }
 
 kotlin {
     compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
 }
 
@@ -37,4 +47,18 @@ dependencies {
     implementation(libs.androidx.compose.animation.core)
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.ui)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "com.github.vedicPrasad"
+                artifactId = "astrology_chart"
+                version = "1.0.0"
+            }
+        }
+    }
 }
